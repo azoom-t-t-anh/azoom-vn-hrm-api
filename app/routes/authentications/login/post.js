a186331bbc483c3cd7360743c0635e162755b4ab
azoom-v-t-dung <v.t.dung@azoom.jp>
2020-03-30 11:00:23 +0700

    create spec to login and logout

--- app/routes/authentications/login/post.js
+++ app/routes/authentications/login/post.js
@@ -1,11 +1,6 @@
 import { generateToken } from '@helpers/jwt-helper'
-import { responseJson } from '@helpers/res-result.js'
-import { getTable } from '@configs/database'
-import { userToken,saveToken } from '@cloudStoreDatabase/token-user'
+import { saveToken } from '@cloudStoreDatabase/token-user'
 import { getUser } from '@cloudStoreDatabase/user'
-
-const _ = require('lodash')
-const bcrypt = require('bcrypt')
 
 export default async function (req, res) {
   try {
@@ -18,15 +13,8 @@ export default async function (req, res) {
         process.env.ACCESS_TOKEN_SECRET,
         process.env.ACCESS_TOKEN_LIFE
       )
-      const refreshToken = await generateToken(
-        user,
-        process.env.REFRESH_TOKEN_SECRET,
-        process.env.REFRESH_TOKEN_LIFE
-      )
-      await saveToken(user.id, user.email, accessToken)
-      responseJson.data = { accessToken, refreshToken }
-      // return res.send("success",responseJson.data)
-      return res.send(responseJson)
+      await saveToken(user.id, accessToken)
+      return res.send({ accessToken: accessToken })
     }
     return res.sendStatus(400)
   } catch (error) {
