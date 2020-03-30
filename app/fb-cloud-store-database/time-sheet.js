55472bc041c85ed8cd170e7d8e722f4c534ba6ee
azoom-v-t-dung <v.t.dung@azoom.jp>
2020-03-30 19:06:30 +0700

    :construction_worker: update checkin and checkout

--- app/fb-cloud-store-database/time-sheet.js
+++ app/fb-cloud-store-database/time-sheet.js
@@ -18,11 +18,11 @@ export const setTimesheetId = () => {
   return date.format(new Date(), 'YYYYMMDD')
 }
 
-export const savetimeSheet = async (user, timeSheetReq) => {
+export const savetimeSheet = async (userId, timeSheetReq) => {
   timeSheetReq.id = setTimesheetId()
-  timeSheetReq.userId = user.id
+  timeSheetReq.userId = userId
   const timeSheet = await getTable(process.env.DB_TABLE_TIME_SHEET)
-    .doc(user.id)
+    .doc(userId)
     .collection(timeSheetReq.id)
     .doc(timeSheetReq.id)
     .set(timeSheetReq)
@@ -45,7 +45,21 @@ export const updateTimesheet = async (userId, timesheetReq) => {
   return !queryData.empty
 }
 
+const checkTimesheetdoc = async userId => {
+  try {
+    const timeSheetId = setTimesheetId(userId)
+    const query = await getTable(process.env.DB_TABLE_TIME_SHEET).get()
+    const result = query.docs.find(doc => (doc.id = timeSheetId))
+    return result ? true : false
+  } catch {
+    return ''
+  }
+}
+
 export const getTimesheetUserday = async (userId, day) => {
+  if (!checkTimesheetdoc(userId)) {
+    return ''
+  }
   const queryData = await getTable(process.env.DB_TABLE_TIME_SHEET)
     .doc(userId)
     .collection(setTimesheetId(date.format(new Date(day), 'YYYYMMDD')))
