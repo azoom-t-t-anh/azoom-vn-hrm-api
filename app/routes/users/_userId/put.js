0ba4d03fc069298c67bd6c428d89c47d4bb153f5
azoom-n-q-hung <54826631+azoom-n-q-hung@users.noreply.github.com>
2020-04-26 16:20:45 +0700

    :tada: Implement put user api #2knexg (#37)
    * :tada: Implement put user api
    
    * Update app/routes/users/_userId/put.js
    
    Co-authored-by: azoom-phan-ha <51304713+azoom-h-phan@users.noreply.github.com>

--- app/routes/users/_userId/put.js
+++ app/routes/users/_userId/put.js
@@ -1,10 +1,21 @@
-import { getUserId } from '@cloudStoreDatabase/user'
+import _ from 'lodash/fp'
+import { userCollection } from '@root/database'
+import { execute } from '@root/util.js'
+import getRole from '@helpers/users/getRole.js'
+import getUser from '@routes/users/_userId/get.js'
 
-// module.exports = async (req, res) => {
-//   const userId = req.params.userId
-//   const user = await getUserId(userId)
-//   if (!user) {
-//     return res.sendStatus(404)
-//   }
-//   return res.send(user)
-// }
+export default async (req, res) => {
+  const { userId } = req.params
+  const { user } = req.body
+  const userUpdateId = req.body.userId
+
+  const role = await getRole(userUpdateId)
+  if (role !== 'admin' && role !== 'editor') return res.sendStatus(403)
+
+  const existUser = await execute(getUser, { params: { userId } })
+  // TODO: Đợi Tuấn Anh fix hàm execute rồi sửa tiếp
+  if (typeof existUser !== 'object') return res.sendStatus(404)
+
+  await userCollection().doc(userId).update(user)
+  return res.send(user)
+}
