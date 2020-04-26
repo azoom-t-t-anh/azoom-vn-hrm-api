e90dc661f197038ef47728717ac236af6d97824b
azoom-n-q-hung <54826631+azoom-n-q-hung@users.noreply.github.com>
2020-04-26 15:59:53 +0700

    :tada: Implement get user list API #2knew7 (#35)
    * :tada: Implement get user list API

--- app/routes/users/get.js
+++ app/routes/users/get.js
@@ -1,8 +1,32 @@
-import { getAllUser } from '@cloudStoreDatabase/user'
+import _ from 'lodash/fp'
+import { userCollection } from '@root/database'
+import getRole from '@helpers/users/getRole.js'
 
-module.exports = async (req, res) => {
-  const pageNumber = parseInt(req.query.pageNumber) || 0
-  const count = parseInt(req.query.count) || ''
+export default async (req, res) => {
+  let { page = 1, limit = 1 } = req.query
+  const { userId } = req.body
 
-  return res.send(await getAllUser(pageNumber, count))
+  const role = await getRole(userId)
+  if (role !== 'admin' && role !== 'editor') return res.sendStatus(403)
+  
+  // TODO: page, limit must be greater than 0  (handle be OpenAPI)
+  // TODO: remove 2 line parser below when openAPI is applied
+  page = parseInt(page)
+  limit = parseInt(limit)
+  const totalIgnoreUser = (page - 1) * limit
+
+  if (totalIgnoreUser === 0) {
+    const users = await userCollection().orderBy('created').limit(limit).get()
+    if (users.empty) return res.send([])
+
+    return res.send(users.docs.map(user => _.omit(['password'], user.data() )))
+  } else {
+    const ignoreUsers = await userCollection().orderBy('created').limit(totalIgnoreUser).get()
+    if (ignoreUsers.empty) return res.send([])
+  
+    const lastIgnoreUser = ignoreUsers.docs[ignoreUsers.docs.length - 1].data()
+    const users = await userCollection().orderBy('created').startAfter(lastIgnoreUser.created).limit(limit).get()
+
+    return res.send(users.docs.map(user => _.omit(['password'], user.data() )))
+  }
 }
