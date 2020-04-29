34c3568c5ae6bfb1750ffcf8b898b3b77abc1527
azoom-t-t-anh <63761811+azoom-t-t-anh@users.noreply.github.com>
2020-04-29 11:12:50 +0700

    :tada: Implement add member to project api #2kp6f7 (#44)
    * :tada: Implement add member to project api
    
    * Update app/routes/projects/_projectId/members/_memberId/get.js
    
    * Update app/routes/projects/_projectId/members/post.js
    
    * Update app/routes/projects/_projectId/members/post.js
    
    * Update app/routes/projects/_projectId/members/post.js
    
    Co-authored-by: azoom-phan-ha <51304713+azoom-h-phan@users.noreply.github.com>

---
+++ app/routes/projects/_projectId/members/_memberId/get.js
@@ -0,0 +1,16 @@
+import { projectCollection } from '@root/database.js'
+
+export default async (req, res) => {
+  const { projectId, memberId } = req.params
+  try {
+    const project = await projectCollection().doc(projectId).get()
+    if (!project.exists) return res.sendStatus(404)
+    const { members } = project.data()
+    if (!members || members.length === 0) return res.sendStatus(404)
+    const memberProfile = members.filter(member => member.memberId === memberId)
+    if (!memberProfile) return res.sendStatus(404)
+    res.send(memberProfile)
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
