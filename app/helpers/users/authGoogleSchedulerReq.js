6ffa0f1f9055ddf4473d0f315814bd93c3b0eed8
Phan Ha <h.phan@azoom.jp>
2020-05-04 17:56:05 +0700

    :bug: Fix authGoogleSchedulerReq

--- app/helpers/users/authGoogleSchedulerReq.js
+++ app/helpers/users/authGoogleSchedulerReq.js
@@ -7,13 +7,13 @@ export default async (authToken) => {
   const token = authToken.split(' ')
   if (token.length < 2) return false
   const response = await got(`${urlVerifyToken}${token}`).catch(() => false)
+  if(!response) return false
   const validateInfo = JSON.parse(response.body)
   const isEmailVerified = validateInfo.email_verified
   const isEmailValid = validateInfo.email === emailValid
   const isTimeExpired = Math.floor(+new Date() / 1000) - validateInfo.exp > 0
   const isAudienceValid = validateInfo.aud === audienceValid
   if (
-    !response ||
     !isEmailVerified ||
     !isEmailValid ||
     isTimeExpired ||
