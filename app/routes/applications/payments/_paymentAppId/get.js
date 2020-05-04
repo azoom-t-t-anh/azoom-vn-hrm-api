5b480de2a086f0316fbd86ec3d575c5c2c8a38ce
azoom-n-t-h-nhung <63988754+azoom-n-t-h-nhung@users.noreply.github.com>
2020-05-04 17:33:05 +0700

    :tada: Implement get payment request detail api (#64)
    * :tada: Implement get payment request detail api
    
    * payment detail example
    
    * Update app/routes/applications/payments/_paymentAppId/get.js
    
    * Update specs/paths/applications/payments/_paymentAppId/get.yaml
    
    Co-authored-by: azoom-phan-ha <51304713+azoom-h-phan@users.noreply.github.com>

---
+++ app/routes/applications/payments/_paymentAppId/get.js
@@ -0,0 +1,14 @@
+import { paymentCollection } from '@root/database'
+import getRole from '@helpers/users/getRole.js'
+
+export default async (req, res) => {
+  const userId = req.user.id
+  const { paymentAppId } = req.query
+
+  const role = await getRole(userId)
+  if(!['admin', 'editor'].includes(role)) return res.sendStatus(403) 
+
+  const paymentDetail = await paymentCollection().doc(paymentAppId).get()
+  if(!paymentDetail.exists) return res.sendStatus(404)
+  return res.send(paymentDetail.data())
+}
