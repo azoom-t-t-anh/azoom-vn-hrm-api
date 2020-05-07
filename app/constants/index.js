8882753fb6262aaa21992a58a72911bb4f1608f0
azoom-n-t-h-nhung <63988754+azoom-n-t-h-nhung@users.noreply.github.com>
2020-05-07 17:19:28 +0700

    :tada: Implement post payment request api (#68)
    * :tada: Implement post payment request api
    
    * user login
    
    * change specs post payment
    
    * format payment id
    
    * fix defaultPaymnent
    
    * fix paymentDefault

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,1 +1,2 @@
 export const status = { inPending: -1, reject: 0, approved: 1 }
+export const paymentIdPrefix = 'payment'
---
+++ app/routes/applications/payments/post.js
@@ -0,0 +1,28 @@
+import { paymentCollection } from '@root/database'
+import getRole from '@helpers/users/getRole.js'
+import { format } from 'date-fns/fp'
+import { paymentIdPrefix } from '@constants/index.js'
+
+export default async (req, res) => {
+  const userId = req.user.id
+  const payment = req.body
+
+  const role = await getRole(userId)
+  if(!['admin', 'editor'].includes(role)) return res.sendStatus(403)
+
+  const id = paymentIdPrefix + '-' + format('yyyyMMddHHmmss', new Date())
+
+  const defaultPayment = {
+    id,
+    userId,
+    status: -1,
+    reason: '',
+    amount: '',
+    isActive: false,
+    created: new Date(),
+    updated: ''
+  }
+  const newPayment = {...defaultPayment, ...payment}
+  await paymentCollection().doc(newPayment.id).set(newPayment)
+  return res.send(newPayment)
+}
