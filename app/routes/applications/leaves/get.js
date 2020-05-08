31715e4e1d936f861da34a57fade8b4664784b9f
azoom-g-v-duc <61811114+azoom-g-v-duc@users.noreply.github.com>
2020-05-08 13:48:42 +0700

    :bugs: Fix missing import from  database (#67)

--- app/routes/applications/leaves/get.js
+++ app/routes/applications/leaves/get.js
@@ -1,16 +1,19 @@
-import { isProjectManager, isAdmin, isEditor } from '@helpers/check-rule'
 import { execute } from '@root/util'
 import getProject from '@routes/projects/get'
+import { leaveApplicationCollection } from '@root/database'
+import getRole from '@helpers/users/getRole'
 
 module.exports = async (req, res) => {
   const pageNumber = parseInt(req.query.pageNumber) || 0
   const count = parseInt(req.query.count) || ''
-
-  if (isAdmin(req.user.positionPermissionId) || isEditor(req.user.positionPermissionId)) {
+  const role = await getRole(req.user.id)
+  if (role === 'admin' || role === 'editor') {
     return res.send(await getAllLeaveAppOfUserList(pageNumber, count, []))
   }
-  if (isProjectManager(req.user.positionPermissionId)) {
-    const projectlist = await execute(getProject, { query: { managerId: req.user.id } })
+  if (role === 'project manager') {
+    const projectlist = await execute(getProject, {
+      query: { managerId: req.user.id },
+    })
     const memberList = projectlist.reduce((members, project) => {
       return {
         ...members,
@@ -26,16 +29,20 @@ module.exports = async (req, res) => {
       )
     )
   }
-  return res.send(await getAllLeaveAppOfUserList(pageNumber, count, [req.user.id]))
+  return res.send(
+    await getAllLeaveAppOfUserList(pageNumber, count, [req.user.id])
+  )
 }
 
 const getAllLeaveAppOfUserList = async (page, number, userIdList) => {
+  
   const result = { count: 0, data: [] }
   let query = await leaveApplicationCollection().orderBy('created', 'desc')
   if (userIdList && userIdList.length) {
     query = query.where('userId', 'in', userIdList)
   }
   const datall = await query.get()
+  
   result.count = datall.empty ? 0 : await datall.docs.length
   if (!page) {
     result.data = datall.empty ? '' : await datall.docs.map((doc) => doc.data())
@@ -43,10 +50,15 @@ const getAllLeaveAppOfUserList = async (page, number, userIdList) => {
   }
   if (page && number && page * number - 1 <= result.count) {
     const queryData = await query
-      .startAt(await datall.docs[page - 1 ? (page - 1) * number : page - 1].data().created)
+      .startAt(
+        await datall.docs[page - 1 ? (page - 1) * number : page - 1].data()
+          .created
+      )
       .limit(number)
       .get()
-    result.data = queryData.empty ? [] : await queryData.docs.map((doc) => doc.data())
+    result.data = queryData.empty
+      ? []
+      : await queryData.docs.map((doc) => doc.data())
     return result
   }
 
